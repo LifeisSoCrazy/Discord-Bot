5a0d2f5cbb93f373918d20dfd68f120cc3da7a7f
Mohammed Rahman 1032 <rahmanmohammed1032@gmail.com>
2022-12-16 11:59:52 -0500

    Registered slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,12 +2,14 @@ const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
-const commands = []
-// Get command files
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith(.js));
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -30,5 +32,3 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.error(error);
 	}
 })();
-
-console.log(commands)
