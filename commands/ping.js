e8cedb203b875189287acae0678751b2ad9219d4
Mohammed Rahman 1032 <rahmanmohammed1032@gmail.com>
2022-12-19 18:54:24 -0500

    Changed commands to be suited towards how it is needed (ephemeral)

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+const wait = require('node:timers/promises').setTimeout;
 module.exports = {
+	// Function that replies 'pong!' when called
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply({ content: 'POOONNNGG', ephemeral: true });
+		await wait(1000);
+		await interaction.followUp({ content: 'AAAAAAAAAAAAAAAAAHHHHHHHHHHHH', ephemeral: true });
 	},
 };
-console.log(execute);
--- commands/user.js
+++ commands/user.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Provides information about the user.'),
-  async execute(interaction) {
-    // Takes information from the interaction object to display user details.
-    await interaction.reply(`User ${interaction.user.username} joined ${interaction.channelid} on ${interaction.member.joined_at}`);
-  },
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about the user.'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, ephemeral: true });
+	},
 };
