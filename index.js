abf7661bb5afd557b417ad0d36dc4c11935ff68e
Mohammed Rahman 1032 <rahmanmohammed1032@gmail.com>
2023-01-03 21:02:21 -0500

    Will be adding a database

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
 // Require the necessary discord.js classes
-// fs gets
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, CommandInteractionOptionResolver } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const token = process.env.DISCORD_TOKEN;
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+// Sequelize instance
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	// SQLite only
+	storage: 'database.sqlite',
 });
 
-// Gets the command directory's path and its individual js files
+/*
+ * equivalent to: CREATE TABLE tags(
+ * name VARCHAR(255) UNIQUE,
+ * description TEXT,
+ * username VARCHAR(255),
+ * usage_count  INT NOT NULL DEFAULT 0
+ * );
+ */
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+});
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.MessageContent] });
+client.commands = new Collection();
+
+// Path for commands
+const commandPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+
+// Loop for adding command files to client.commands
+for (const file of commandFiles) {
+	const filePath = path.join(commandPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	// eslint-disable-next-line brace-style
+	} else {
+		console.log(`${filePath} does not have required "data" or "execute property"`);
+	}
+}
+// Gets the event directory's path and its individual js files for events directory
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
+	// Get file path of the events
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
 	if (event.once) {
+		// Sync the model
+		Tags.sync({ force: true });
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
-
 // Log in to Discord with your client's token
 client.login(token);
